ffc010fc9c380f00c7cd7c5c019b45f78943dbbb
Jaegu Kang <jaegu@datamarketing.co.kr>
2021-05-21 00:33:14 +0900

    위메프 크롤러 완성

---
+++ backend/crawler/wemakeprice.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+def get_wmp_data():
+    """
+    이름, 가격, 썸네일(url)
+    상품(url), 평점, 평점 개수
+    """
+    keyword = input('검색어 입력하세요..\n')
+
+    url = f'https://search.wemakeprice.com/api/wmpsearch/api/v3.0/wmp-search/search.json?searchType=DEFAULT&search_cate=top&keyword={keyword}&isRec=1&_service=5&_type=3&page=1'
+    res = requests.get(url)
+    data = res.json()['data']['deals']
+    result = [
+        {
+            'title': item['dispNm'],
+            'image': item['originImgUrl'],
+            'price': item['originPrice'],
+            'salePrice': item['salePrice'],
+            'discountRate': item['discountRate'],
+            'reviewAvgPoint': item['reviewAvgPoint'],
+            'reviewCount': item['reviewCount']
+        }
+        for item in data
+    ]
+    return result[:20]
+
+result = get_wmp_data()
+
+result = {
+    'wemakeprice': result
+}
+
+pprint(result)

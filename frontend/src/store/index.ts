2e808d458dd5628c3a06a8f2d076beed3254e440
sangbong-an <jahae854@gmail.com>
2021-07-22 00:26:53 +0900

    delete : 불필요 리듀서 정리

--- frontend/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { numberReducer } from './mallReducer';
-
-const reducers = combineReducers({
-  numberReducer,
-});
-
-export default reducers;
-export type RootState = ReturnType<typeof reducers>;

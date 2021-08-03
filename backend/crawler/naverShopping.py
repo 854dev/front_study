24a9e25c4e4571edaa785526cf74a5b83adf0a3a
Dev_taeyoon <taeyoon@datamarketing.co.kr>
2021-08-03 19:24:39 +0900

    crawler return changed

--- backend/crawler/naverShopping.py
+++ backend/crawler/naverShopping.py
@@ -1,15 +1,31 @@
 import requests
-from bs4 import BeautifulSoup as bs
-keyword = input('Input the keyword to find: ')
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import json
+
 
 def naver_shopping_crawl(keyword):
     base_url = 'https://search.shopping.naver.com/search/all'
     # query=자전거&cat_id=&frm=NVSHATC
-    session = requests.session()
-    session.headers['upgrade-insecure-requests']=1
-    session.headers['']
-    param = {
-        'query':keyword,
-        'cat_id':None,
-        'frm':'NVSHATC'
+    ua = UserAgent()
+    url = 'https://search.shopping.naver.com/search/all'
+    headers = {
+        'user-agent': ua.random
     }
+    param = {
+        'query': keyword,
+        'cat_id': None,
+        'frm': 'NVSHATC'
+    }
+    res = requests.get(url,headers=headers,params=param)
+    html = BeautifulSoup(res.text, 'html.parser')
+    title_list = html.select('#__next > div > div.style_container__1YjHN > div.style_inner__18zZX > div.style_content_wrap__1PzEo > div.style_content__2T20F > ul > div > div > li > div > div.basicList_info_area__17Xyo > div.basicList_title__3P9Q7 > a')
+    print(title_list)
+    more_list = html.select('#__NEXT_DATA__')
+    text = more_list[0].get_text().replace('<script id="__NEXT_DATA__" type="application/json">','').replace('</script>','')
+    data_json = json(text)
+    print(data_json)
+if __name__=="__main__":
+    naver_shopping_crawl("애플")
+
+
--- backend/main.py
+++ backend/main.py
@@ -25,36 +25,27 @@ def read_root():
 @app.get("/call_crawl/coupang")
 async def read_item(query: str):
     coupang_result = coupang.coupang_crawler(query)
-    res_param = {
-        'coupang': coupang_result,
-    }
-    return res_param
+    return coupang_result
 
 
 @app.get("/call_crawl/daaangn")
 async def read_item(query: str):
     daaangn_result = daaangn.daaangn_crawler(query)
     # wemakeprice_result = wemakeprice.get_wmp_data(query)
-    res_param = {
-        'daaangn': daaangn_result,
-    }
-    return res_param
+    return daaangn_result
 
 
 @app.get("/call_crawl/homenshopping")
 async def read_item(query: str):
     homenshopping_result = homenshopping.get_hns_data(query)
     # wemakeprice_result = wemakeprice.get_wmp_data(query)
+    return homenshopping_result
+
+
+@app.get("/call_crawl/wemakeprice")
+async def read_item(query: str):
+    wemakeprice_result = wemakeprice.get_wmp_data(query)
     res_param = {
-        'homenshopping': homenshopping_result
+        'wemakeprice': wemakeprice_result
     }
     return res_param
-
-#
-# @app.get("/call_crawl/wemakeprice")
-# async def read_item(query: str):
-#     wemakeprice_result = wemakeprice.get_wmp_data(query)
-#     res_param = {
-#         'wemakeprice': wemakeprice_result
-#     }
-#     return res_param
